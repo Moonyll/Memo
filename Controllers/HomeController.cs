21f7ce62e431b413eef4c446205347b767f6c4d6
Moonyll <s.pouwels@laposte.net>
2019-08-29 10:04:25 +0200

    1st commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Memo.Models;
+namespace Memo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet] // 1er appel de la méthode - on indique GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost] // On soumet le formulaire - On indique POST.
+        public ActionResult Index(float nb1, float nb2, string operand)
+        {
+            Operation calculate = new Operation();
+            calculate.x = nb1; // 1er nombre
+            calculate.y = nb2; // 2nd nombre
+            calculate.op = operand; // Opérateur
+            calculate.Calculate(); // Calcul
+            ViewBag.result = calculate.result; // Affichage du résultat
+            return View();
+        }
+    }
+}
---
+++ Models/Memos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memo.Models
+{
+    public class Operation
+    {
+        public float x { get; set; } // 1er nombre
+        public float y { get; set; } // 2nd nombre
+        public string op { get; set;} // Opérateur
+        public string result { get; set; } // Résultat
+        public string Calculate() // Méthode de calcul
+        {
+            return result =
+                (op == "+") ? (x + y).ToString() : // Addition
+                (op == "-") ? (x - y).ToString() : // Soustraction
+                (op == "*") ? (x * y).ToString() : // Multiplication
+                (op == "/" && y != 0) ? (x / y).ToString() : " Div by 0..."; // Division
+        }
+    }
+}
